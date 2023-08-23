4ce2a32e73efcec814ffd613d06f5ba90e7d1056
maheshkloudspot <mahesh.bishnoi@kloudspot.com>
2023-08-23 15:14:00 +0530

    add cross origin

--- amserver/src/main/java/com/kloudspot/controller/AssetController.java
+++ amserver/src/main/java/com/kloudspot/controller/AssetController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import com.kloudspot.service.IAssetService;
  */
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/api/v1/assets") // Base endpoint for all asset-related operations
 public class AssetController {
 
